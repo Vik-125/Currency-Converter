ed9ae60bcc0358f47d43a47fd743f2262603d3d5
vik <theheero007@Gmail.com>
2026-03-24 16:28:26 +0530

    Added multiple currencies for conversion

--- lib/currency_convertor_material_page.dart
+++ lib/currency_convertor_material_page.dart
@@ -1,5 +1,6 @@
-import 'package:http/http.dart' as http; // this lib is used to make a web req(API call).
-import 'dart:convert';          // This lib is used to convert the JSON data into dart readable data
+import 'package:http/http.dart'
+    as http; // this lib is used to make a web req(API call).
+import 'dart:convert'; // This lib is used to convert the JSON data into dart readable data
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
@@ -14,51 +15,87 @@ class CurrencyConvertorMaterialPage extends StatefulWidget {
   State<CurrencyConvertorMaterialPage> createState() =>
       _CurrencyConvertorMaterialPageState();
 }
+
 class _CurrencyConvertorMaterialPageState
     extends State<CurrencyConvertorMaterialPage> {
   double result = 0;
   String resultText = "";
+  String fromCurrency = "INR";
+  String toCurrency = "USD";
+  final List<String> currencies = [
+    "AUD",
+    "BRL",
+    "CAD",
+    "CHF",
+    "CNY",
+    "CZK",
+    "DKK",
+    "EUR",
+    "GBP",
+    "HKD",
+    "HUF",
+    "IDR",
+    "ILS",
+    "INR",
+    "ISK",
+    "JPY",
+    "KRW",
+    "MXN",
+    "MYR",
+    "NOK",
+    "NZD",
+    "PHP",
+    "PLN",
+    "RON",
+    "SEK",
+    "SGD",
+    "THB",
+    "TRY",
+    "USD",
+    "ZAR",
+  ];
   final TextEditingController textEditingController = TextEditingController();
 
-  Future<void> convertCurrency() async{
-  final String inputText = textEditingController.text;
+  Future<void> convertCurrency() async {
+    final String inputText = textEditingController.text;
+    final double? inputAmt = double.tryParse(inputText);
 
-  final double? inputAmt = double.tryParse(inputText);
-
-  if(inputAmt == null){
-    setState(() {
-      resultText = "That not a Number , you DUMB!!!";
-    });
-    return;
-  }
-  if(inputAmt == 0){
-    setState(() {
-      resultText = "Broke!!!";
-    });
-    return;
-  }
-
-  try{
-    final url = Uri.parse('https://api.frankfurter.app/latest?base=USD');
-
-    final response = await http.get(url);
-
-    if(response.statusCode == 200){
-      final Map<String, dynamic> data = jsonDecode(response.body);
-
-      final double exchangeRate = data['rates']['INR'];
-
-      setState((){
-        resultText = (inputAmt*exchangeRate).toString();
+    if (inputAmt == null) {
+      setState(() {
+        resultText = "That not a NUMBER , you DUMB!!!";
       });
-    } else {
-        if(kDebugMode) print('Server error: ${response.statusCode}');
-      }
+      return;
     }
-    catch(e){
-      if(kDebugMode) print('Error : $e');
+    if (inputAmt <= 0) {
+      setState(() {
+        resultText = "Broke!!!";
+      });
+      return;
+    }
+
+    try {
+      final url = Uri.parse(
+        'https://api.frankfurter.app/latest?base=$fromCurrency',
+      );
+
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = jsonDecode(response.body);
+
+        final double exchangeRate = data['rates'][toCurrency];
+
+        setState(() {
+          resultText = "${(inputAmt * exchangeRate).toString()} $toCurrency";
+        });
+      } else {
+        if (kDebugMode) print('Server error: ${response.statusCode}');
+      }
+    } catch (e) {
+      if (kDebugMode) print('Error : $e');
     }
   }
+
   @override
   Widget build(BuildContext context) {
     // here we have mentioned Border function , so that we can use it below in the code.
@@ -85,7 +122,9 @@ class _CurrencyConvertorMaterialPageState
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              resultText.isNotEmpty ? resultText : '₹${result!=0 ? result.toStringAsFixed(2) : 0}',
+              resultText.isNotEmpty
+                  ? resultText
+                  : '₹${result != 0 ? result.toStringAsFixed(2) : 0}',
               style: TextStyle(
                 fontSize: 32.2,
                 fontWeight: FontWeight.bold,
@@ -94,11 +133,51 @@ class _CurrencyConvertorMaterialPageState
             ),
             Padding(
               padding: const EdgeInsets.all(18.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  DropdownButton<String>(
+                    value: fromCurrency,
+                    items: currencies
+                        .map(
+                          (cur) =>
+                              DropdownMenuItem(value: cur, child: Text(cur)),
+                        )
+                        .toList(),
+                    onChanged: (val) => setState(() => fromCurrency = val!),
+                  ),
+
+                  const Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20.0, 
+                      right: 20.0, 
+                      top: 0.0,
+                      bottom: 0.0,
+                    ),
+                    child: Icon(Icons.arrow_forward),
+                  ),
+
+                  DropdownButton<String>(
+                  
+                    value: toCurrency,
+                    items: currencies
+                        .map(
+                          (cur) =>
+                              DropdownMenuItem(value: cur, child: Text(cur)),
+                        )
+                        .toList(),
+                    onChanged: (val) => setState(() => toCurrency = val!),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(18.0),
               child: TextField(
                 controller: textEditingController,
                 style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
                 decoration: InputDecoration(
-                  hintText: "Enter amount in USD",
+                  hintText: "Enter amount",
                   hintStyle: const TextStyle(
                     color: Color.fromARGB(255, 0, 0, 0),
                   ),
@@ -115,7 +194,7 @@ class _CurrencyConvertorMaterialPageState
               ),
             ),
             // Button
-    
+
             // raised
             // appears
             TextButton(
